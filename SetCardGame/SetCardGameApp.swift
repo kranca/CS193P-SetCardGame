8b31f4b7117d70e8a1b4bd530f25460b9a1c05c6
kranca <90696292+kranca@users.noreply.github.com>
2022-03-15 09:55:54 +0100

    Initial Commit

---
+++ SetCardGame/SetCardGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  SetCardGameApp.swift
+//  SetCardGame
+//
+//  Created by Raúl Carrancá on 15/03/22.
+//
+
+import SwiftUI
+
+@main
+struct SetCardGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
