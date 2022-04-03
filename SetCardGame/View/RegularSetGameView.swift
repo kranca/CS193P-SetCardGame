e4371d571237f9a76ac5c773b93afab3978e1e1b
kranca <90696292+kranca@users.noreply.github.com>
2022-04-03 22:54:42 +0200

    Worked on dealing animation
    Worked on dealing animation. Now working for cards leaving the board, but not for cards arriving.

--- SetCardGame/View/RegularSetGameView.swift
+++ SetCardGame/View/RegularSetGameView.swift
@@ -8,10 +8,28 @@
 import SwiftUI
 
 struct RegularSetGameView: View {
+    typealias Card = SetGame<RegularSetGame.CardContent>.Card
+    
     @ObservedObject var game = RegularSetGame()
     @Namespace private var dealingNamespace
     
     @State private var deckDealt = Set<UUID>()
+    
+    private func deal(_ card: Card) {
+        deckDealt.insert(card.id)
+    }
+    
+//    private func isUndealt(_ card: Card) -> Bool {
+//        !deckDealt.contains(card.id)
+//    }
+    
+    private func dealAnimation(for card: Card) -> Animation {
+        var delay = 0.0
+        if let index = game.cardsOnBoard.firstIndex(where: { $0.id == card.id }) {
+            delay = Double(index) * (3 / Double(game.cardsOnBoard.count))
+        }
+        return Animation.easeInOut(duration: 1).delay(delay)
+    }
     
     var body: some View {
         ZStack(alignment: .bottom) {
@@ -33,6 +51,7 @@ struct RegularSetGameView: View {
         VStack {
             AspectRatioVGrid(items: game.cardsOnBoard, aspectRatio: 2/3, content: { card in
                 cardView(for: card)
+                    .matchedGeometryEffect(id: card.id, in: dealingNamespace)
                     .onTapGesture {
                         withAnimation(.easeInOut) {
                             game.choose(card)
@@ -66,6 +85,11 @@ struct RegularSetGameView: View {
         .frame(width: 60, height: 90)
         .onTapGesture {
             game.deal()
+            for card in game.cardsOnBoard {
+                withAnimation(dealAnimation(for: card)) {
+                    deal(card)
+                }
+            }
         }
     }
     
@@ -84,12 +108,12 @@ struct RegularSetGameView: View {
         
     }
     
-    private func zIndex(of card: SetGame<RegularSetGame.CardContent>.Card) -> Double {
+    private func zIndex(of card: Card) -> Double {
         -Double(game.deck.firstIndex(where: { $0.id == card.id }) ?? 0)
     }
     
     @ViewBuilder
-    func cardView(for card: SetGame<RegularSetGame.CardContent>.Card) -> some View {
+    func cardView(for card: Card) -> some View {
         Group {
             if let match = card.isMatched {
                if match {
